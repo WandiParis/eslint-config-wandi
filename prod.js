2182c8ba256ad43fcd3c3532530622dcb18977fe
Cyrille Perois <cyrille.perois@wandi.fr>
2017-01-19 15:45:13 +0100

    Split dev/prod (default dev)

---
+++ prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: "./dev.js",
+    rules: {
+        "no-console": "error",
+        "no-debugger": "error",
+        "no-empty": "error",
+        "no-unreachable": "error",
+        "no-alert": "error"
+    }
+}
